86a4f88be7c9aa17322edf06e40838a57a36f0c1
Alan Geller <ageller@microsoft.com>
2020-08-06 16:14:50 -0700

    Basic structure

---
+++ src/Simulation/Microsoft.Quantum.TargetGeneration/Context.fs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Quantum.QsCompiler.TargetGeneration
+
+open Microsoft.CodeAnalysis
+open Microsoft.CodeAnalysis.CSharp.Syntax
+open Microsoft.CodeAnalysis.Formatting
+
+open Microsoft.Quantum.RoslynWrapper
+
+open System
+open System.Collections.Generic
+open System.IO
+open Microsoft.CodeAnalysis
+open Microsoft.Quantum.QsCompiler
+open Microsoft.Quantum.QsCompiler.CsharpGeneration
+open Microsoft.Quantum.QsCompiler.DataTypes
+open Microsoft.Quantum.QsCompiler.Diagnostics
+open Microsoft.Quantum.QsCompiler.ReservedKeywords
+open Microsoft.Quantum.QsCompiler.SyntaxTree
+open Microsoft.Quantum.QsCompiler.Transformations.BasicTransformations
+open System.Collections.Immutable
+
+module Utils =
+    let namespaceElementIsIntrinsic elem =
+        match elem with
+        | QsCallable callable -> 
+            if callable.Specializations |> Seq.exists (fun spec -> match spec.Implementation with
+                                                                   | Intrinsic -> true
+                                                                   | _ ->         false)
+            then Some callable
+            else None
+        | _ -> None
+
+    let findIntrinsics syntaxTree =
+        syntaxTree 
+        |> Seq.collect (fun ns -> ns.Elements |> Seq.choose namespaceElementIsIntrinsic)
+
+open Utils
+open GenerateTarget
+
+type TargetGenerationContext =
+    {
+        assemblyConstants   : IDictionary<string, string>
+        intrinsics          : IEnumerable<QsCallable>
+    }
+    with
+
+    static member Create(syntaxTree, assemblyConstants) =
+        { assemblyConstants = assemblyConstants; intrinsics = syntaxTree |> findIntrinsics }
+
+    static member Create(syntaxTree) =
+        { assemblyConstants = ImmutableDictionary.Empty; intrinsics = syntaxTree |> findIntrinsics }
+
+    member this.GenerateTarget(filePath) =
+        ()
---
+++ src/Simulation/Microsoft.Quantum.TargetGeneration/RewriteStep.fs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Quantum.QsCompiler.TargetGeneration
+
+open System
+open System.Collections.Generic
+open System.IO
+open Microsoft.CodeAnalysis
+open Microsoft.Quantum.QsCompiler
+open Microsoft.Quantum.QsCompiler.CsharpGeneration
+open Microsoft.Quantum.QsCompiler.DataTypes
+open Microsoft.Quantum.QsCompiler.Diagnostics
+open Microsoft.Quantum.QsCompiler.ReservedKeywords
+open Microsoft.Quantum.QsCompiler.SyntaxTree
+open Microsoft.Quantum.QsCompiler.Transformations.BasicTransformations
+
+
+type Emitter() =
+
+    let _AssemblyConstants = new Dictionary<_, _>()
+    let mutable _Diagnostics = []
+
+    interface IRewriteStep with
+
+        member this.Name = "TargetGeneration"
+        member this.Priority = -1 // doesn't matter because this rewrite step is the only one in the dll
+        member this.AssemblyConstants = upcast _AssemblyConstants
+        member this.GeneratedDiagnostics = upcast _Diagnostics
+        
+        member this.ImplementsPreconditionVerification = false
+        member this.ImplementsPostconditionVerification = false
+        member this.ImplementsTransformation = true
+
+        member this.PreconditionVerification _ = NotImplementedException() |> raise
+
+        member this.PostconditionVerification _ = NotImplementedException() |> raise
+        
+        member this.Transformation (compilation, transformed) = 
+            let step = this :> IRewriteStep
+            let dir = step.AssemblyConstants.TryGetValue AssemblyConstants.OutputPath |> function
+                | true, outputFolder when outputFolder <> null -> outputFolder
+                | _ -> step.Name
+
+            let context = TargetGenerationContext.Create(compilation.Namespaces, step.AssemblyConstants)
+            context.GenerateTarget(Path.Combine(dir, "Target.cs"))
+
+            transformed <- compilation
+            true
